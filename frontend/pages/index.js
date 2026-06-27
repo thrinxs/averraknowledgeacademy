07518e3a8c7832fb9d6e6048933b1a5aff9ccd33
Thrinxs <info.thrinxs@gmail.com>
2026-06-27 02:37:35 +0100

    fix: remove stray frontend folder causing Vercel routing conflict

--- frontend/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to Averra Knowledge Academy</h1>
-        <p>Your gateway to digital skills, career training, and more.</p>
-      </div>
-    );
-  }
